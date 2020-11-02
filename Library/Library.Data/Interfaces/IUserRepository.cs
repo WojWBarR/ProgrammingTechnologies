e766c36e58def97b570a787ddd474fd0d6426121
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-02 20:15:45 +0100

    Adding implementation of User repository interface

--- Library/Library.Data/Interfaces/IUserRepository.cs
+++ Library/Library.Data/Interfaces/IUserRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface IUserRepository
     {
+        List<User> GetAllUsers();
+        User GetUserById(int id);
+        void DeleteUser(int id);
+        void EditUser(User user);
+        void AddUser(User user);
     }
 }
