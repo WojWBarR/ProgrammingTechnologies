6c30439874fd709e7fe276c9a21e6eae26e36f1c
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-02 20:07:34 +0100

    Adding implementation of Book repository interface

--- Library/Library.Data/Interfaces/IBookRepository.cs
+++ Library/Library.Data/Interfaces/IBookRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface IBookRepository
     {
+        List<Book> GetAllBooks();
+        Book GetBookById(int id);
+        Book GetBookByType(BookEnum bookType);
+        void DeleteBook(int id);
+        void EditBook(Book book);
+        void AddBook(Book book);
     }
 }
