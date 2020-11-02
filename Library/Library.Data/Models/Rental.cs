1d1fbb933b5d4f89b652318e510c76946c8725ce
wojtas112 <65348511+wojtas112@users.noreply.github.com>
2020-11-02 19:53:46 +0100

    Implementation of class Rental in Models

--- Library/Library.Data/Models/Rental.cs
+++ Library/Library.Data/Models/Rental.cs
@@ -6,5 +6,10 @@ namespace Library.Data.Models
 {
     public class Rental
     {
+        public int Id { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime GiveBackDate { get; set; }
+        public User RentalUser { get; set; }
+        public OurLibrary LibraryBooks { get; set; }
     }
 }
