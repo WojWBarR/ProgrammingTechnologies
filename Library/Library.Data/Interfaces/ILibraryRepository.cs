abddda5a44c6f9a888a963809d42787e4c6c4000
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-02 20:09:27 +0100

    Adding implementation of Library repository interface

--- Library/Library.Data/Interfaces/ILibraryRepository.cs
+++ Library/Library.Data/Interfaces/ILibraryRepository.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface ILibraryRepository
     {
+        List<Book> GetAllAvailableBooks();
+        List<Book> GetAllUnavailableBooks();
+        void AddBookWithChangedState(Book book);
     }
 }
