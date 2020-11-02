78dc3ed2db591e6510dbf67f6532746f7de2b198
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-02 20:12:53 +0100

    Adding implementation of Rental repository interface

--- Library/Library.Data/Interfaces/IRentalRepository.cs
+++ Library/Library.Data/Interfaces/IRentalRepository.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface IRentalRepository
     {
+        List<Rental> GetAllRentals();
+        Rental GetRentalById(int id);
+        void DeleteRental(int id);
+        void EditRental(Rental rental);
+        void AddRental(Rental rental);
+        void AddUserToRentalHistory(User user);
     }
 }
