9b7fce3fa53262c092b759b73771fc304571d978
wojtas112 <65348511+wojtas112@users.noreply.github.com>
2020-11-02 19:48:03 +0100

    Implementation of class Book in Models

--- Library/Library.Data/Models/Book.cs
+++ Library/Library.Data/Models/Book.cs
@@ -6,5 +6,9 @@ namespace Library.Data.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public BookEnum BookType { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
