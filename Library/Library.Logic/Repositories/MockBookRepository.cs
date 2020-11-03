a880aa294e5e278f4dcc4cbbc9bb2a9f97fe82e2
wojtas112 <65348511+wojtas112@users.noreply.github.com>
2020-11-03 17:33:03 +0100

    Adding namespaces to MockBookRepository class

--- Library/Library.Logic/Repositories/MockBookRepository.cs
+++ Library/Library.Logic/Repositories/MockBookRepository.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Library.Data.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data;
+using Library.Data.Models;
 
 namespace Library.Logic.Repositories
 {
