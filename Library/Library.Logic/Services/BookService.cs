9b1c85efb21349d5c3e5b4ab767aa56ef813b05c
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-03 11:02:57 +0100

    Adding implementation of Book service

--- Library/Library.Logic/Services/BookService.cs
+++ Library/Library.Logic/Services/BookService.cs
@@ -1,10 +1,65 @@
-﻿using System;
+﻿using Library.Data;
+using Library.Data.Interfaces;
+using Library.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Library.Logic.Services
 {
-    class BookService
+    public class BookService
     {
+        private readonly IBookRepository bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            List<Book> books = bookRepository.GetAllBooks();
+
+            return books.Count == 0 ? null : books;
+        }
+
+        public Book GetBook(int id)
+        {
+            Book book = bookRepository.GetBookById(id);
+
+            if (book.Equals(null))
+            {
+                return null;
+            }
+
+            return book;
+        }
+
+        public Book GetBook(BookEnum bookType)
+        {
+            Book book = bookRepository.GetBookByType(bookType);
+
+            if (book.Equals(null))
+            {
+                return null;
+            }
+
+            return book;
+        }
+
+        public void DeleteBook(int id)
+        {
+            bookRepository.DeleteBook(id);
+        }
+
+        public void EditBook(Book book)
+        {
+            bookRepository.EditBook(book);
+        }
+
+        public void AddBook(Book book)
+        {
+            bookRepository.AddBook(book);
+        }
     }
 }
