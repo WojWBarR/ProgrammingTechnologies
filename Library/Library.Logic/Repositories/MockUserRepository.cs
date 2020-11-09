176179d5af5e3c4a7a2acf8981aa0eaece5e717d
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-09 16:48:21 +0100

    Adding implementation of User repository

--- Library/Library.Logic/Repositories/MockUserRepository.cs
+++ Library/Library.Logic/Repositories/MockUserRepository.cs
@@ -1,10 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Interfaces;
+using Library.Data.Models;
 
 namespace Library.Logic.Repositories
 {
-    public class MockUserRepository
+    public class MockUserRepository : IUserRepository
     {
+        private List<User> users;
+
+        public MockUserRepository()
+        {
+            users = new List<User>
+            {
+                new User {Id = 1, Name = "aaa", Surname = "aaaa", AmountOfBooksRented = 1},
+                new User {Id = 2, Name = "bbb", Surname = "bbb", AmountOfBooksRented = 4},
+                new User {Id = 3, Name = "ccc", Surname = "ccc", AmountOfBooksRented = 3},
+                new User {Id = 4, Name = "ddd", Surname = "ddd", AmountOfBooksRented = 1},
+                new User {Id = 5, Name = "eee", Surname = "eee", AmountOfBooksRented = 4},
+                new User {Id = 6, Name = "fff", Surname = "fff", AmountOfBooksRented = 6},
+            };
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return users;
+        }
+
+        public User GetUserById(int id)
+        {
+            return users.FirstOrDefault(i => i.Id.Equals(id));
+        }
+
+        public void DeleteUser(int id)
+        {
+            User deletedUser = users.FirstOrDefault(i => i.Id.Equals(id));
+
+            if (deletedUser != null)
+            {
+                users.Remove(deletedUser);
+            }
+        }
+
+        public void EditUser(User user)
+        {
+            User editedUser = users.FirstOrDefault(b => b.Id.Equals(user.Id));
+
+            if (editedUser != null)
+            {
+                editedUser.Id = user.Id;
+                editedUser.Name = user.Name;
+                editedUser.Surname = user.Surname;
+                editedUser.AmountOfBooksRented = user.AmountOfBooksRented;
+            }
+        }
+
+        public void AddUser(User user)
+        {
+            User addedUser = new User
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                AmountOfBooksRented = user.AmountOfBooksRented
+            };
+
+            users.Add(addedUser);
+        }
     }
 }
