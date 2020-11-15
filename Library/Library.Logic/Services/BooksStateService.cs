a68562f1a4e616c797fc8a482a00a52261f62689
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-15 19:50:00 +0100

    Adding implementation of Book State

---
+++ Library/Library.Logic/Services/BooksStateService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Library.Data.Interfaces;
+using Library.Data.Models;
+
+namespace Library.Logic.Services
+{
+    public class BooksStateService
+    {
+        private readonly IBooksStateRepository booksStateRepository;
+
+        public BooksStateService(IBooksStateRepository booksStateRepository)
+        {
+            this.booksStateRepository = booksStateRepository;
+        }
+
+        public List<BooksCatalog.Book> GetAllAvailableBooks()
+        {
+            var availableBooks = booksStateRepository.GetAllAvailableBooks();
+
+            return availableBooks.Count == 0 ? null : availableBooks;
+        }
+
+        public int GetAmountOfAvailableBooks(int id)
+        {
+            var amount = booksStateRepository.GetAmountOfAvailableBooksById(id);
+            return amount;
+        }
+
+        public void UpdateBooksAmount(int bookId, int actualBooksAmount)
+        {
+            booksStateRepository.UpdateBooksAmount(bookId, actualBooksAmount);
+        }
+    }
+}
