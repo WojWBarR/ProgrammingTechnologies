cb3ddef5829fc3e6b99d6841b80a75a76bfe6ae3
wojtas112 <65348511+wojtas112@users.noreply.github.com>
2020-11-15 21:37:13 +0100

    uploading implementation of empty test classes

--- Library/Library.Tests/UnitTests/RepositoriesTests/BookEventRepositoryTests.cs
+++ Library/Library.Tests/UnitTests/RepositoriesTests/BookEventRepositoryTests.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Library.Data.Models;
+using Library.Logic;
+using Library.Logic.Repositories;
+using Xunit;
 
 namespace Library.Tests.UnitTests.RepositoriesTests
 {
-    class BookEventRepositoryTests
+    public class BookEventRepositoryTests
     {
+        public BookEventRepositoryTests()
+        {
+            dbContext = new MockDbContext();
+            bookEventRepository = new MockBookEventRepository(dbContext);
+        }
+
+        private readonly MockBookEventRepository bookEventRepository;
+        private readonly MockDbContext dbContext;
+
+        [Fact]
+        public void ShouldAddRentalEvent()
+        {
+            //Arrange
+            var rentalEvent = new RentalEvent();
+
+            //Act
+            bookEventRepository.AddRentalEvent(rentalEvent);
+
+            //Assert
+        }
+
+        [Fact]
+        public void ShouldAddReturnEvent()
+        {
+            //Arrange
+            var returnEvent = new ReturnEvent();
+
+            //Act
+            bookEventRepository.AddReturnEvent(returnEvent);
+
+            //Assert
+        }
+
+        [Fact]
+        public void ShouldReturnAllEvents()
+        {
+            //Arrange
+
+            //Act
+            var returnedRentals = bookEventRepository.GetAllBookEvents();
+
+            //Assert
+            Assert.True(returnedRentals.Count.Equals(3));
+        }
     }
 }
--- Library/Library.Tests/UnitTests/RepositoriesTests/BooksCatalogRepositoryTests.cs
+++ Library/Library.Tests/UnitTests/RepositoriesTests/BooksCatalogRepositoryTests.cs
@@ -1,10 +1,148 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using Library.Data;
+using Library.Data.Models;
+using Library.Logic;
+using Library.Logic.Repositories;
+using Xunit;
 
 namespace Library.Tests.UnitTests.RepositoriesTests
 {
-    class BooksCatalogRepositoryTests
+    public class BooksCatalogRepositoryTests
     {
+        public BooksCatalogRepositoryTests()
+        {
+            dbContext = new MockDbContext();
+            booksCatalogRepository = new MockBooksCatalogRepository(dbContext);
+        }
+
+        private readonly MockBooksCatalogRepository booksCatalogRepository;
+        private readonly MockDbContext dbContext;
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(6)]
+        public void ShouldReturnBookById(int id)
+        {
+            //Arrange
+            var booksCatalog = new BooksCatalog
+            {
+                Books = new List<BooksCatalog.Book>
+                {
+                    new BooksCatalog.Book {Id = 1, Title = "aaaa", BookType = BookEnum.Adventure},
+                    new BooksCatalog.Book {Id = 3, Title = "cccc", BookType = BookEnum.Document},
+                    new BooksCatalog.Book {Id = 6, Title = "ffff", BookType = BookEnum.Document}
+                }
+            };
+
+            //Act
+            var returnedBook = booksCatalogRepository.GetBookById(id);
+
+            //Assert
+            switch (id)
+            {
+                case 1:
+                    Assert.Equal(booksCatalog.Books[0].Id, returnedBook.Id);
+                    break;
+                case 3:
+                    Assert.Equal(booksCatalog.Books[1].Id, returnedBook.Id);
+                    break;
+                case 6:
+                    Assert.Equal(booksCatalog.Books[2].Id, returnedBook.Id);
+                    break;
+            }
+        }
+
+        [Theory]
+        [InlineData(BookEnum.Adventure)]
+        [InlineData(BookEnum.Document)]
+        [InlineData(BookEnum.Roman)]
+        public void ShouldReturnBookByBookType(BookEnum bookType)
+        {
+            //Arrange
+            var booksCatalog = new BooksCatalog
+            {
+                Books = new List<BooksCatalog.Book>
+                {
+                    new BooksCatalog.Book {Id = 1, Title = "aaaa", BookType = BookEnum.Adventure},
+                    new BooksCatalog.Book {Id = 3, Title = "cccc", BookType = BookEnum.Document},
+                    new BooksCatalog.Book {Id = 5, Title = "ffff", BookType = BookEnum.Roman}
+                }
+            };
+
+            //Act
+            var returnedBook = booksCatalogRepository.GetBookByType(bookType);
+
+            //Assert
+            switch (bookType)
+            {
+                case BookEnum.Adventure:
+                    Assert.Equal(booksCatalog.Books[0].BookType, returnedBook.BookType);
+                    break;
+                case BookEnum.Document:
+                    Assert.Equal(booksCatalog.Books[1].BookType, returnedBook.BookType);
+                    break;
+                case BookEnum.Roman:
+                    Assert.Equal(booksCatalog.Books[2].BookType, returnedBook.BookType);
+                    break;
+            }
+        }
+
+        [Fact]
+        public void ShouldAddBook()
+        {
+            //Arrange
+            var booksCatalog = new BooksCatalog
+            {
+                Books = new List<BooksCatalog.Book>
+                    {new BooksCatalog.Book {Id = 7, Title = "aaaa", BookType = BookEnum.Historic}}
+            };
+
+            //Act
+            booksCatalogRepository.AddBook(booksCatalog.Books[0]);
+
+            //Assert
+        }
+
+        [Fact]
+        public void ShouldDeleteBook()
+        {
+            //Arrange
+
+            //Act
+            booksCatalogRepository.DeleteBook(1);
+
+            var books = booksCatalogRepository.GetAllBooks();
+
+            //Assert
+        }
+
+        [Fact]
+        public void ShouldEditBook()
+        {
+            //Arrange
+            var booksCatalog = new BooksCatalog
+            {
+                Books = new List<BooksCatalog.Book>
+                    {new BooksCatalog.Book {Id = 1, Title = "aaaa", BookType = BookEnum.Adventure}}
+            };
+
+            //Act
+            booksCatalogRepository.EditBook(booksCatalog.Books[0]);
+
+            //Assert
+        }
+
+        [Fact]
+        public void ShouldReturnAllBooks()
+        {
+            //Arrange
+
+            //Act
+            var returnedBooks = booksCatalogRepository.GetAllBooks();
+
+            //Assert
+            Assert.True(returnedBooks.Count.Equals(6));
+        }
     }
 }
