5a493599e7b22c25d89c337433fd9bda5f50c963
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-15 19:51:01 +0100

    Adding implementation of BookEventService

---
+++ Library/Library.Logic/Services/BooksCatalogService.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Library.Data;
+using Library.Data.Interfaces;
+using Library.Data.Models;
+
+namespace Library.Logic.Services
+{
+    public class BooksCatalogService
+    {
+        private readonly IBooksCatalogRepository booksCatalogRepository;
+
+        public BooksCatalogService(IBooksCatalogRepository booksCatalogRepository)
+        {
+            this.booksCatalogRepository = booksCatalogRepository;
+        }
+
+        public void AddBook(BooksCatalog.Book book)
+        {
+            booksCatalogRepository.AddBook(book);
+        }
+
+        public void DeleteBook(int id)
+        {
+            booksCatalogRepository.DeleteBook(id);
+        }
+
+        public void EditBook(BooksCatalog.Book book)
+        {
+            booksCatalogRepository.EditBook(book);
+        }
+
+        public List<BooksCatalog.Book> GetAllBooks()
+        {
+            var books = booksCatalogRepository.GetAllBooks();
+
+            return books.Count == 0 ? null : books;
+        }
+
+        public BooksCatalog.Book GetBook(int id)
+        {
+            var bookById = booksCatalogRepository.GetBookById(id);
+
+            if (bookById.Equals(null)) return null;
+
+            return bookById;
+        }
+
+        public BooksCatalog.Book GetBook(BookEnum bookType)
+        {
+            var bookByType = booksCatalogRepository.GetBookByType(bookType);
+
+            if (bookByType.Equals(null)) return null;
+
+            return bookByType;
+        }
+    }
+}
