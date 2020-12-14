a5ba9dd55be007c0e894df5915a1d601e88216a1
Wojciech <65348511+wojtas112@users.noreply.github.com>
2020-12-14 21:08:46 +0100

    Create BookDictionary class in Models

---
+++ Library/Library.Data/Models/BookDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Library.Data.Models
+{
+    public class BookDictionary
+    {
+        [Key]
+        public int DictionaryId { get; set; }
+        public Book Book { get; set; }
+        public int BooksAmount { get; set; }
+    }
+}
