58252dd7cc2d68a37e6f999e033e0bd55fa97582
Wojciech <65348511+wojtas112@users.noreply.github.com>
2020-12-14 22:05:01 +0100

    implementing userlistviewmodel

--- Library/Library.Logic/ViewModels/UserListViewModel.cs
+++ Library/Library.Logic/ViewModels/UserListViewModel.cs
@@ -1,10 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Library.Data;
+using Library.Logic.Commands;
 
 namespace Library.Logic.ViewModels
 {
     public class UserListViewModel : BaseViewModel
     {
+        private User _selectedUser;
+        private BaseViewModel _selectedViewModel;
+        private UserRepository _userRepository = new UserRepository();
+        private ObservableCollection<User> _users;
+
+        public RelayCommand LoadDataCommand { get; set; }
+        public RelayCommand DeleteCommand { get; set; }
+        public ICommand UpdateViewCommand { get; set; }
+
+        public UserListViewModel()
+        {
+            Task.Run(() => { Users = new ObservableCollection<User>(UserRepository.GetAllUsers()); });       
+            LoadDataCommand = new RelayCommand(() => UserRepository = new UserRepository());
+            DeleteCommand = new RelayCommand(Delete, CanDelete);
+
+        }
+
+        public ObservableCollection<User> Users
+        {
+            get { return _users; }
+            set
+            {
+                _users = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                RaisePropertyChanged();
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public UserRepository UserRepository
+        {
+            get { return _userRepository; }
+            set
+            {
+                _userRepository = value;
+                Users = new ObservableCollection<User>(value.GetAllUsers());
+            }
+        }
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public void Delete()
+        {
+            Task.Factory.StartNew(() => _userRepository.DeleteUser(SelectedUser.Id))
+                .ContinueWith((t1) => UserRepository = _userRepository);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedUser != null;
+        }
     }
 }
